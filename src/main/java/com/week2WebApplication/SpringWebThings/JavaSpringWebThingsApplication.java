4846504f7d86d0e53d345d34d0e57821e7e47eb0
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-09-21 11:25:30 +0530

    Initialized and set up all the things

---
+++ src/main/java/com/week2WebApplication/SpringWebThings/JavaSpringWebThingsApplication.java
@@ -0,0 +1,13 @@
+package com.week2WebApplication.SpringWebThings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringWebThingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringWebThingsApplication.class, args);
+	}
+
+}
