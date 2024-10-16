782ba8d6f2b6c91b31c463420a472e47d61c22b1
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-10-16 20:12:30 +0530

    @requestMapping annotation and required field

--- src/main/java/com/week2WebApplication/SpringWebThings/controllers/EmployeeController.java
+++ src/main/java/com/week2WebApplication/SpringWebThings/controllers/EmployeeController.java
@@ -3,6 +3,7 @@ package com.week2WebApplication.SpringWebThings.Controllers; //This is from pull
 import com.week2WebApplication.SpringWebThings.DTO.EmployeeDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 //her we can write a logic for employee controller
 @RestController
@@ -18,5 +19,14 @@ public class EmployeeController {
     @GetMapping(path = "/{empid}")
     public EmployeeDTO EmployeeID(@PathVariable int empid){
         return new EmployeeDTO(empid, "7666947497", "Pallavi Purushottam", "Pallavis Heart", 27000, "pediatricks", "DrPallavi.Darade@pgutthe.com" );
+    }
+    @GetMapping(path = "/Optional/{emp}")
+    public EmployeeDTO employeeDTO(@RequestParam(required = false) int emp){
+        return new EmployeeDTO(emp, "8605566761","Purushottam","Gokarna Palace, Kalyani nagar Pune", 7352000,"Business","purushottamgutthe6767@gmail.com");
+    }
+    @GetMapping(path = "/Details")
+    public String OptionalParam(@RequestParam(required = false) int age,
+                                @RequestParam(required = false) String Name){
+        return "Hi "+Name+", Your age is "+age;
     }
 }
